abea1f567ac9dc664503d3fb77d4910ec63d761b
Puskor Roy <puskarkumarroy329@gmail.com>
2025-04-04 00:33:21 +0600

    test html work

--- event/urls.py
+++ event/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from event.views import hello
+from event.views import hello,test,search_view
 
 urlpatterns = [
-    path("hello/",hello)
+    path("hello/",hello),
+    path("test/",test),
+    
+    path('search/', search_view, name='search')
+    
 ]
--- event/views.py
+++ event/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 def hello(request):
     return HttpResponse("hello boyes")
+
+def test(request):
+    return render(request,"test.html")
+
+def search_view(request):
+    query = request.GET.get('q', '')
+    results = []  # Replace with actual search logic (database query, etc.)
+    return render(request, 'search.html', {'query': query, 'results': results})
 
