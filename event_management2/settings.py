1331a48f2ad730c2d78f647c1fb24a2dd5860ef4
Puskor Roy <puskarkumarroy329@gmail.com>
2025-04-14 16:03:56 +0600

    change database

--- event_management2/settings.py
+++ event_management2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,15 +85,23 @@ WSGI_APPLICATION = 'event_management2.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_management2',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_management2',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_managemet2_user:1ndXaz7Wed1qIn2FNLRqsZRr1yzFefNA@dpg-cvudkbmuk2gs7389rn60-a.oregon-postgres.render.com/event_managemet2',
+        conn_max_age=600
+    )
 }
 
 INTERNAL_IPS = [
