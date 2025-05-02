98a1c7b7b071327a8967b842deb6355be7514a9e
Puskor Roy <puskarkumarroy329@gmail.com>
2025-05-02 21:54:46 +0600

    add 2nd time

--- users/signals.py
+++ users/signals.py
@@ -12,8 +12,7 @@ def send_activation_email(sender, instance, created, **kwargs):
         token = default_token_generator.make_token(instance)
         activation_url = f"{settings.FRONTEND_URL}/users/activate/{instance.id}/{token}/"
         subject = 'Activate Your Account'
-        message = f'Hi {instance.username},\n\nPlease activate your account by clicking the link below:\n{
-            activation_url}\n\nThank You!'
+        message = f'Hi {instance.username},\n\nPlease activate your account by clicking the link below:\n{activation_url}\n\nThank You!'
         recipient_list = [instance.email]
 
         try:
