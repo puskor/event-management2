0c0ffb6342b461ec01d7892edc531fd9aeb7f941
Puskor Roy <puskarkumarroy329@gmail.com>
2025-05-16 21:48:30 +0600

    added sql lite

--- event_management2/settings.py
+++ event_management2/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = 'event_management2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
@@ -110,13 +110,13 @@ WSGI_APPLICATION = 'event_management2.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://event_managemet2_user:1ndXaz7Wed1qIn2FNLRqsZRr1yzFefNA@dpg-cvudkbmuk2gs7389rn60-a.oregon-postgres.render.com/event_managemet2',
-        conn_max_age=600
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://event_managemet2_user:1ndXaz7Wed1qIn2FNLRqsZRr1yzFefNA@dpg-cvudkbmuk2gs7389rn60-a.oregon-postgres.render.com/event_managemet2',
+#         conn_max_age=600
+#     )
+# }
 
 INTERNAL_IPS = [
     # ...
